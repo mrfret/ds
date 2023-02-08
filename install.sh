06b3fe17998e7441fb7a4660d7f866932f47889f
mrfret <72273384+mrfret@users.noreply.github.com>
2023-02-08 15:16:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,19 +27,18 @@ ds=/opt/ds
 
 git clone https://github.com/mrfret/ds.git /opt/ds 1>/dev/null 2>&1
 
-if test -f "/usr/bin/ds"; then $(which rm) /usr/bin/ds ; fi
 if [[ $EUID != 0 ]]; then
     $(which chown) -R $(whoami):$(whoami) /opt/ds
     $(which usermod) -aG sudo $(whoami)
-    cp /opt/ds/apps.sh ./apps.sh
-    chmod +x ~./apps.sh
-    $(which chown) $(whoami):$(whoami) ~./apps.sh
+    cp /opt/ds/apps.sh /apps.sh
+    chmod +x /apps.sh
+    $(which chown) $(whoami):$(whoami) /apps.sh
 else 
     $(which chown) -R 1000:1000 /opt/ds
-    cp /opt/ds/apps.sh ./apps.sh
-    chmod +x ./apps.sh
+    cp /opt/ds/apps.sh /apps.sh
+    chmod +x /apps.sh
 	chmod +x /opt/ds/*.sh
-    $(which chown) -R 1000:1000 ./apps.sh
+    $(which chown) -R 1000:1000 /apps.sh
 fi
 
 #EOF#
